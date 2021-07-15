d0ac9f164d1b24115673c00c3f999ab99fe1780a
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-07-15 00:33:33 -0500

    modified server and added comments to code to ensure that proxy is working properly with doing the fetch calls with the external API and working receiving the fetch calls from the panda-aja-prototype app

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 'use strict'
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-app.set('port', port)
-const request = require('request');
-// const fetch = require('node-fetch');
+// express is an npm package for node https://www.npmjs.com/package/express this makes it possible for this server to listen to specific queries 
+//made from the specified port and other things such as .use() method to set the headers to avoid cors issues.
 
-// MIDDLEWARE (transform stream)
+
+const express = require('express')
+// you assign express to variable app
+const app = express()
+// here you are saying if port is not specified used whatever defaulted port or 3000
+const port = process.env.PORT || 3000
+// you set the port to express so that at the very bottom it can listen to it
+app.set('port', port)
+// installed node-fetch because request is deprecated
+const fetch = require('node-fetch');
+
+// MIDDLEWARE (transform stream): allows data to be received by app with no CORS errors.
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -15,73 +22,86 @@ app.use(function(req, res, next) {
 });
 
 
+//.get() is also a method from express, here you are telling the server to perform an action when the "passcode" (/api/translate) is received
 app.get('/api/translate', (req, res) => {
-  // const query = `&location=${req.query.location}&radius=${req.query.radius}`
-  const key = "$key=e12f972de0msh5f3353dde969f09p1be95bjsna98e93807839"
+  // over in panda-aja! prototype, the app fetched to this server by doing a fetch('localhost:3000/api/translate?&language=ko&phrase="woah") the values after the "?"
+  // are sent here as an object which we can access using the parameter of req, req.query and then by using dot notation you can access each value
   const url = `https://microsoft-translator-text.p.rapidapi.com/translate?api-version=3.0&to=${req.query.language}&textType=plain&profanityAction=NoAction&from=en`
+  console.log('req', req.query, 'call', url);
+  // those values are not restricted to be used in the url, you can also use them anywhere you'd like inside this .get() function, and so
+  // you are putting the phrase in the body.
   const body = [
 		{
 			"Text": `${req.query.phrase}`
 		}
 	]
 
-  console.log("body", body, req.query);
-  console.log("call made to api", req.query.language)
-  console.log("hello", url);
-  // let lang = `${req.query.language}`
-
-  request.post(
-    {
-       url: `${url}`,
-       headers: {
-              "content-type": "application/json",
-		          "x-rapidapi-key": "e12f972de0msh5f3353dde969f09p1be95bjsna98e93807839",
-		          "x-rapidapi-host": "microsoft-translator-text.p.rapidapi.com"
-       },
-       body: JSON.stringify(body),
+  // The code below is fetching to the microsoft translation external api. The url above is being attached to the fetch below and being passed are
+  //also the headers that rapid api provides, which works as authentication
+    fetch(`${url}`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "x-rapidapi-key": "e12f972de0msh5f3353dde969f09p1be95bjsna98e93807839",
+        "x-rapidapi-host": "microsoft-translator-text.p.rapidapi.com",
+        "Accept": '*/*'
       },
-      function (error, response, body) {
-        // console.log('error', error);
-        // console.log('response', response);
-        // console.log('body', body);
-        res.send(body); 
-      }
-    );
+      body: JSON.stringify(body)
+  })
+  .then((response) => response.json())
+  .then((data) => res.send(data));
 
 });
 
 app.get('/api/transliterate', (req, res) => {
-  // const query = `&location=${req.query.location}&radius=${req.query.radius}`
-  const url = `https://microsoft-translator-text.p.rapidapi.com/transliterate?api-version=3.0&language=${req.query.language}&fromScript=Kore&toScript=Latn`
+  const url = `https://microsoft-translator-text.p.rapidapi.com/transliterate?api-version=3.0&language=${req.query.language}&fromScript=${req.query.transliterationCode}&toScript=Latn`
+  console.log('req', req.query, 'call', url);
   const body = [
     {"Text": `${req.query.phrase}`}
   ]
-
-  console.log("body", body, req.query);
-  console.log("call made to api", req.query.language)
-  console.log("hello", url);
-  // let lang = `${req.query.language}`
-
-  request.post(
-    {
-       url: `${url}`,
-       headers: {
-              "content-type": "application/json",
-		          "x-rapidapi-key": "e12f972de0msh5f3353dde969f09p1be95bjsna98e93807839",
-		          "x-rapidapi-host": "microsoft-translator-text.p.rapidapi.com"
-       },
-       body: JSON.stringify(body),
+    fetch(`${url}`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "x-rapidapi-key": "e12f972de0msh5f3353dde969f09p1be95bjsna98e93807839",
+        "x-rapidapi-host": "microsoft-translator-text.p.rapidapi.com",
+        "Accept": '*/*'
       },
-      function (error, response, body) {
-        // console.log('error', error);
-        // console.log('response', response);
-        // console.log('body', body);
-        res.send(body); 
-      }
-    );
+      body: JSON.stringify(body)
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('data in proxy', data);
+    res.send(data)
+  });
 
 });
 
+app.get('/api/detect', (req, res) => {
+  const url = `https://microsoft-translator-text.p.rapidapi.com/Detect?api-version=3.0`
+  console.log('req', req.query, 'call', url);
+  const body = [
+    {"Text": `${req.query.phrase}`}
+  ]
+    fetch(`${url}`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "x-rapidapi-key": "e12f972de0msh5f3353dde969f09p1be95bjsna98e93807839",
+        "x-rapidapi-host": "microsoft-translator-text.p.rapidapi.com",
+        "Accept": '*/*'
+      },
+      body: JSON.stringify(body)
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('data in proxy', data);
+    res.send(data)
+  });
+
+});
+
+// here express is listening to the port for those passcode queries example: /api/translate
 app.listen(port, () =>
   console.log(`Listening on port: ${port}`)
 )
